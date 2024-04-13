02eea552c2747918f923884edd02babd1848093c
drenushashoshi <ds68479@ubt-uni.net>
2024-04-13 23:05:55 +0200

    Create a base for DatabaseConnection
    Created the base for database connection. One of the first tasks assigned, tho its not tested nor finished without creating entities and controllers first.

---
+++ ApplicationDbContext.cs
@@ -0,0 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+
+    // Add your entity DbSet properties here
+}
