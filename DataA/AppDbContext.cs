3afcc355fa03ae9ad3a29cfab5f82a8fcd191043
erblink <118224412+erblink@users.noreply.github.com>
2024-04-28 20:32:48 +0200

    Created a constructor that doesn't accept parameters, to mock the Db, added virtual to Movie and User DbSet

--- DataA/AppDbContext.cs
+++ DataA/AppDbContext.cs
@@ -3,19 +3,22 @@ using movie_rating_backend.Entity;
 
 public class AppDbContext : DbContext
 {
+
+   
+    public AppDbContext() { }
     public AppDbContext(DbContextOptions<AppDbContext> options)
-        : base(options)
+     : base(options)
     {
     }
 
-    public DbSet<Movie> Movies { get; set; }
+
+    public virtual DbSet<Movie> Movies { get; set; }
     public DbSet<Category> Categories {  get; set; }
-    public DbSet<User> Users {  get; set; }
+    public virtual DbSet<User> Users {  get; set; }
     public DbSet<Rating>? Ratings { get; set; }
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.Entity<Rating>().HasKey(x => new { x.MovieId, x.UserId });
     }
-
 
 }
